1a1db4f4d0263f94831bc19356b67203ea2e1d7c
mouzkolit <mouzkolit@outlook.de>
2023-04-07 21:51:16 +0200

    added database to the analysis, now every search will be stored as a table in a duckdb database, in addition we will allow for aggregated analysis and also will provide an author search

--- connector/DataBaseConnector.py
+++ connector/DataBaseConnector.py
@@ -1,54 +1,59 @@
 import duckdb
 import os
+import pandas as pd
 
 # Class to write the searched strings in database
 # This should reduce the search terms
 #
 
 class DatabaseConnector:
-    
-    def __init__(self) -> None:
+    # This is the Database connector singleton
+    # This should be used to write the data to the database
+    # This should be used to read the data from the database
+    # This should be used to check if the data is already in the database
+    # This should be used to check if the database is already in the path
+    def __init__(self, read_only: bool = False) -> None:
         self.database_name: str = "PubMedDataBase.duckdb"
+        self.read_only: bool = read_only
         self.con = None
         if not self.check_database():
             self.create_database()
-        self.write_database()
-            
-    def write_database(self):
-        
+        else:
+            self.con = duckdb.connect(database=self.database_name, read_only=self.read_only)
+
+    def write_database(self, table: pd.DataFrame, table_name: str, search_key: str):
+
         """This should write the database with the key (pubmed search key)
         and the acquired data
-        
+
         DataBase holds a table with keys and the associated table name
         in addition tables with the data will be created
         """
-        pass
-    
+        try:
+            table_df = table #.to_sql(table_name, self.con, if_exists="replace")
+            table_name = "_".join(table_name.split(" ")) # create a table name without spaces
+            self.con.execute(f"CREATE TABLE {table_name} AS SELECT * FROM table_df")
+            self.con.execute("INSERT INTO PubMedKeys (key, table_name) VALUES (?, ?)", (search_key, table_name))
+        except Exception as e:
+            print(e)
+        finally:
+            self.con.close() # close the connection
+
     def check_database(self) -> bool:
         """_summary_: This should check if the database is already in the path
 
         Returns:
-            bool: _description_
+            bool: Returns if database is found in the path or not
         """
         return self.database_name in os.listdir()
-    
+
     def create_database(self):
         """This creates the database
         """
         self.con = duckdb.connect(database=self.database_name, read_only=False)
-        
-    
-    def write_table_into_database(self):
-        """_summary_
-        """
-        q = "CREATE TopicsMap (ID INT, SearchTerm VARCHAR, Time TIMESTAMP, date DATE UNIQUE)"
-        self.con.execute(q)
-        
-        
-    def show_all_tables(self):
-        """Tnhis should return all tables
+        create_unique_offline_analysis_sequence = """CREATE SEQUENCE ID;"""
+        self.con.execute(create_unique_offline_analysis_sequence)
+        self.con.execute("""CREATE TABLE PubMedKeys(ID_Pub integer PRIMARY KEY DEFAULT(nextval ('ID')),
+                                                    key VARCHAR,
+                                                    table_name VARCHAR)""")
 
-        Returns:
-            list: _description_
-        """
-        return self.con.execute("SHOW TABLES").fetchall()
--- pages/1_Pubmed_Processing.py
+++ pages/1_Pubmed_Processing.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import re 
+import re
 import os
 import openai
-from gensim.parsing.preprocessing import preprocess_string,strip_tags, strip_punctuation, remove_stopwords
+from gensim.parsing.preprocessing import preprocess_string, remove_stopwords
 from gensim.models import Phrases
 from gensim.models.doc2vec import TaggedDocument
 from gensim import corpora, models
@@ -13,13 +13,12 @@ from sklearn.neighbors import kneighbors_graph
 import leidenalg as la
 import igraph
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-import matplotlib
-import seaborn as sns
+
 from bertopic import BERTopic
 from connector.PubMedConnector import PubMedConnector
 from connector.DataBaseConnector import DatabaseConnector
+from summarizer import Summarizer
+
 
 @st.cache
 def load_data():
@@ -27,25 +26,18 @@ def load_data():
     current_path = os.getcwd()
     return pd.read_csv(current_path + "/data/impact_factor.txt", delimiter = "\t")
 
-def load_database():
-    database = DatabaseConnector("pubmed.db")
-    database.open_database()
-    return database
-
-@st.cache
-def openai_init():
-    openai.api_key = os.getenv('OPENAI_KEY')
-    
 # make a list of stopwords
 def processing_data():
-    """_summary_:  Get the impact factor list 
+    """_summary_:  Get the impact factor list
 
     Args:
         impact_factor (pd.DataFrame): DataFrame of impact factors
     """
     # layout of the streamlit page that is necessary for the training
+
     st.sidebar.subheader("Choose your search term")
     if url := st.sidebar.text_input('Enter your search query here:'):
+        database = DatabaseConnector(False)
         #database.write_mapping_table(url)
         stopword_list = ["among","although","especially","kg","km","mainly","ml","mm",
                         "disease","significantly","obtained","mutation","significant",
@@ -55,12 +47,20 @@ def processing_data():
                         "suprisingly","preferentially","subsequently","far","little","known","importantly",
                         "synonymous","skipping","father","mother","pedigree","novo","rescues","rescued","restored",
                         "exhibits","induce", "Background","Objective","Methods","cells", "kinase","activation","protein"]
+        try:
+            abstract_data, tab3 = retrieve_abstract_data(url, stopword_list)
+            database.write_database(abstract_data, url, url)
+            bert_topic_modelling(abstract_data,tab3)
+        except Exception as e:
+            print(e)
+        finally:
+            # This is important in case of a failure and therefore database is not close which kind of disrupts the app
+            database.con.close()
 
-        abstract_data, tab3 = retrieve_abstract_data(url, stopword_list)
-        bert_topic_modelling(abstract_data,tab3)
 
 def retrieve_abstract_data(url, stopword_list):
-    """_summary
+    """_summary: Retrieves available abstract data from Pubmed
+    using multiprocessing and saves this into a result data frame
 
     Args:
         url (str): Search Term in Pubmed
@@ -73,10 +73,10 @@ def retrieve_abstract_data(url, stopword_list):
     pubmed_fetch = PubMedConnector(url)
     result = pubmed_fetch.query_data()
     return process_abstract_data(result,stopword_list)
-    
-    
+
+
 def process_abstract_data(result, stopword_list):
-    """_summary_: 
+    """_summary_:
 
     Args:
         result (_type_): _description_
@@ -85,7 +85,7 @@ def process_abstract_data(result, stopword_list):
     Returns:
         _type_: _description_
     """
-    
+
     result["date"] = [int(i[:4]) for i in result["Publication_date"].tolist()]
     tab1, tab2, tab3, tab4 = st.tabs(["Overview","Abstract Umap","Content Analysis", "Summarizer"])
 
@@ -94,12 +94,12 @@ def process_abstract_data(result, stopword_list):
     tab1_col1, tab1_col2 = tab1.columns(2)
 
         #tab1 is the overview tab
-    
+
     count_occurences_per_year(result, tab1_col1)
     count_occurences_per_journal(result, tab1_col2)
     top_10_first_authors(result, tab1_col1)
     top_10_last_authors(result, tab1_col2)
-    
+
     #preprocessing of the abstracts that are searched for, remove punctuations, stopwords
     result["processed_abstracts"] = preprocessing_list(result["abstract"], stopword_list)
     bigram = Phrases(result["processed_abstracts"].tolist(),min_count = 5, threshold = 100)
@@ -116,14 +116,14 @@ def process_abstract_data(result, stopword_list):
         df_umap["abstract"] = result["abstract"].tolist()
         umap_figure = draw_umap(df_umap)
         tab2.plotly_chart(umap_figure, use_container_width = True)
-        summarize_ai_text(df_umap, tab4)
+        #summarize_bert_text(df_umap, tab4)
 
     result["labels"] = df_umap["labels"]
     return result, tab3
-            
+
 def preprocessing_list(liste,stopword_list):
     """ preprocess the abstract list, remove stopwords, punctuations, numbers
-    input: 
+    input:
     liste: list of abstracts
     stopword_list: list of stopwords
     returns"""
@@ -137,7 +137,6 @@ def preprocessing_list(liste,stopword_list):
         no_integers = [re.sub("[^A-Za-z0-9|-]","",x) for x in no_integers]
         processed_abstracts.append(no_integers)
     return processed_abstracts
-
 
 @st.cache
 def running_model(document):
@@ -153,7 +152,7 @@ def running_model(document):
         min_count=4,
         epochs = 5,
         workers = 8,
-        dm=0, 
+        dm=0,
         dbow_words=1)
     model_stream.train(document,total_examples=model_stream.corpus_count,epochs=model_stream.epochs)
     return model_stream
@@ -167,9 +166,9 @@ def umap_visualization(model):
     Returns:
         _type_: _description_
     """
-    doc_tags = model.dv.vectors 
+    doc_tags = model.dv.vectors
     X = doc_tags
-    
+
     prediction_labels = umap.UMAP(n_neighbors=5,
                                     min_dist=0.0,
                                     n_components=2,
@@ -217,22 +216,22 @@ def count_occurences_per_year(abstract_df,tab):
     tab.plotly_chart(year_plot, use_container_width = True)
 
 def count_occurences_per_journal(abstract_df,tab):
-    """_summary_
+    """_summary_: Retrieves the aggregated counts per journal
 
     Args:
         abstract_df (pd.DataFrame): the dataframe hodling all the abstracts
         tab (st.col): streamlit tab column
     """
     grouped_occurences = abstract_df.groupby("Journal")["Journal"].count().sort_values(ascending = False).iloc[:10]
-    journal_plot = px.bar(grouped_occurences, 
-                          x=grouped_occurences.index, 
+    journal_plot = px.bar(grouped_occurences,
+                          x=grouped_occurences.index,
                           y=grouped_occurences.values,
                           title = "# Publication per Journal"
                           )
     tab.plotly_chart(journal_plot, use_container_width = True)
 
 def top_10_first_authors(abstract_df,tab):
-    """_summary_
+    """_summary_: Retrieves the top 10 first authors from the abstract
 
     Args:
         abstract_df (pd.DataFrame): dataframe holding all the abstract
@@ -246,16 +245,16 @@ def top_10_first_authors(abstract_df,tab):
     tab.plotly_chart(first_plot, use_container_width = True)
 
 def top_10_last_authors(abstract_df,tab):
-    """_summary_
+    """_summary_: retrieves the top10 last authors
 
     Args:
         abstract_df (pd.DataFrame): _description_
         tab (st.col): _description_
     """
     grouped_occurences = abstract_df.groupby("last")["last"].count().sort_values(ascending = False).iloc[:10]
-    last_plot = px.bar(grouped_occurences, 
-                       x=grouped_occurences.index, 
-                       y=grouped_occurences.values, 
+    last_plot = px.bar(grouped_occurences,
+                       x=grouped_occurences.index,
+                       y=grouped_occurences.values,
                        title = "Top 10 last authors")
     tab.plotly_chart(last_plot, use_container_width = True)
 
@@ -273,22 +272,22 @@ def bert_topic_modelling(df_end, tab):
     topic_model = BERTopic(nr_topics="auto")
     topics, probs = topic_model.fit_transform(df_end["string_abstract"].tolist())
     topics_per_class = topic_model.topics_per_class(df_end["string_abstract"].tolist(), classes=targets)
-    
-    col1, col2 = tab.columns(2)
-    col1.plotly_chart(topic_model.visualize_topics(), use_container_width = True)
-    col2.plotly_chart(topic_model.visualize_topics_per_class(topics_per_class), use_container_width = True)
-   
-    
+
+    #col1, col2 = tab.columns(2)
+    #col1.plotly_chart(topic_model.visualize_topics(), use_container_width = True)
+    tab.plotly_chart(topic_model.visualize_topics_per_class(topics_per_class), use_container_width = True)
+
+
 def write_table_model(model_list):
     """
-
     Args:
         model_list (_type_): _description_
     """
+
     pass
 
 def summarize_ai_text(results, tab):
-    """_summary_
+    """_summary_: This can summarize the text using the OPEN AI gpt model
 
     Args:
         results (pd.DataFrame): _description_
@@ -307,10 +306,27 @@ def summarize_ai_text(results, tab):
                             max_tokens=1000,
                                 )
         summaries_dict[i].append(summary)
-        
-    
+        break
+
+def summarize_bert_text(results, tab):
+    """_summary_: This can summarize the text using the Bert Models
+
+    Args:
+        results (pd.DataFrame): Holding the Abstract Data
+        tab (st.Tabs): _description_
+    """
+    summaries_dict = {} # should hold the summaries
+    model= Summarizer() # creates the summarizer model
+    for i in results["labels"].unique():
+        df_text = results[results["labels"]==i]
+        text = ".".join(df_text["abstract"].tolist()) # should be the label text
+        summary = model(text, ratio = 0.05)
+        print(summary)
+        summaries_dict[i].append(summary)
+        break
+
+
 if __name__ == "__main__":
     impact_factor = load_data()
-    openai_init()
-    #database = load_database()
     processing_data()
+
